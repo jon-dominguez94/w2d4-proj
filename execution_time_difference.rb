6f8b66dd79602a0012aa3949db544243d8b2acee
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-01 14:26:36 -0700

    Write #largest_contiguous_sums methods

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -18,6 +18,8 @@ end
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list)
 
+#time complexity : quadratic O(n**2)
+
 def better_my_min(array)
   smallest = array[0]
   array[1..-1].each do |el|
@@ -27,4 +29,38 @@ def better_my_min(array)
 end
 
 p better_my_min(list)
+
+#time complexity: linear: O(n)
+
+def largest_contiguous_subsum(array)
+  sub_arrays = []
+  i = 1
+  until i == array.length + 1
+    array.each_cons(i) do |sub|
+      sub_arrays << sub
+    end
+    i += 1
+  end
+  sub_arrays.map {|sub| sub.reduce(:+)}.max
+end
+
+list = [5,3,-7]
+p largest_contiguous_subsum(list)
+
+
+def better_largest_contiguous_subsum(array)
+  largest_sum = array[0]
+  i = 1
+  until i == array.length + 1
+    array.each_cons(i) do |sub|
+      sum = sub.reduce(:+)
+      largest_sum = sum if largest_sum < sum
+    end
+    i += 1
+  end
+  largest_sum
+end
+
+p better_largest_contiguous_subsum(list)
+
 
