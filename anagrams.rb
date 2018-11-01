aacc99d9bbaf35a3541baea67ca26bd02b0de6b1
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-01 15:32:05 -0700

    Write #second_anagram method

--- anagrams.rb
+++ anagrams.rb
@@ -1,4 +1,4 @@
-def inefficient_anagram(string, test_str)
+def first_anagram?(string, test_str)
   arr = string.chars.permutation(test_str.length)
   arr.each do |possible_match|
     return true if possible_match == test_str.chars
@@ -6,5 +6,19 @@ def inefficient_anagram(string, test_str)
   false
 end
 
-p inefficient_anagram("gizmo", "sally")    #=> false
-p inefficient_anagram("elvis", "lives")    #=> true
+p first_anagram?("gizmo", "sal")    #=> false
+p first_anagram?("elvis", "lives")    #=> true
+
+
+def second_anagram?(string, test_str)
+  test_str_arr = test_str.chars
+  string.chars.each do |ch|
+    index = test_str_arr.index(ch)
+    return false if index.nil?
+    test_str_arr.delete_at(index)
+  end
+  test_str_arr.empty?
+end
+
+p second_anagram?("gizmo", "sal")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
